234d2218ec6ee55323d25267172ba607450aeccf
jamie-legg <legg.jamie@gmail.com>
2020-01-27 00:46:35 +0000

    deploy fix

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
-function redirectUnmatched(req, res) {
-  res.redirect("https://jamielegg.com/gethub");
-}
 
-app.use(redirectUnmatched);
 app.use(express.static(path.join(__dirname, 'build')));
 
 
@@ -16,4 +12,7 @@ app.get('/gethub', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 80, () => {
+  console.log("Listener online");
+  
+});
